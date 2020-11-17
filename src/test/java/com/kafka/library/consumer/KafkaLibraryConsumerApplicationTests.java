727e1adc26f54d9d5b3b9853e517bc4abb677c6b
breno500as <breno500as@gmail.com>
2020-11-17 14:55:00 -0300

    kafka

---
+++ src/test/java/com/kafka/library/consumer/KafkaLibraryConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.library.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLibraryConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
