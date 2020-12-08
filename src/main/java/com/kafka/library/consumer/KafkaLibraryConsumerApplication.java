1bbc067fcd328f937d87b828141f17921e2b65fe
breno500as <breno500as@gmail.com>
2020-12-08 15:56:26 -0300

    kafka

--- src/main/java/com/kafka/library/consumer/KafkaLibraryConsumerApplication.java
+++ src/main/java/com/kafka/library/consumer/KafkaLibraryConsumerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KafkaLibraryConsumerApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaLibraryConsumerApplication.class, args);
 	}
