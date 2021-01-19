62917710edecb8dac6b1a9419a9893d01e3e2d29
breno500as <breno500as@gmail.com>
2021-01-18 22:37:17 -0300

    kafka

--- src/main/java/com/kafka/library/consumer/KafkaLibraryConsumerApplication.java
+++ src/main/java/com/kafka/library/consumer/KafkaLibraryConsumerApplication.java
@@ -3,14 +3,16 @@ package com.kafka.library.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author breno
+ *
+ */
 @SpringBootApplication
 public class KafkaLibraryConsumerApplication {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaLibraryConsumerApplication.class, args);
 	}
-	
+
 }
-
-
